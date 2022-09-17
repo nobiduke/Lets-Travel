34e84fd60922de1a5857864998ed904a9e1cc239
nobiduke <lmjones647@gmail.com>
2022-09-16 23:52:14 -0400

    map size

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const displacement = (774, 345);
 
+const CalcX = (baseX) => {
+    return (-1*baseX + displacement[0]);
+}
+const CalcY = (baseY) => {
+    return (-1*baseY + displacement[1]);
+}
+
 let elem = document.getElementById('world')
 
-setTimeout(Move, 3000)
 
-const Move = () => {
-    elem.style.left = '0px'
-    elem.style.top = '0px'
+const Move = (country) => {
+    elem.style.left = CalcX(country.X);
+    elem.style.top = CalcY(country.Y);
 }
+
+setTimeout(Move({x:'', y:''}), 3000);
