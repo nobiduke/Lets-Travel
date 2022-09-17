653ac29bc6fe2f899aaefc52d5c00a9a662b1fbe
nobiduke <lmjones647@gmail.com>
2022-09-17 00:23:14 -0400

    movement

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-const displacement = (774, 345);
+const displacement = [774, 345];
 
 const CalcX = (baseX) => {
+    baseX = parseInt(baseX);
     return (-1*baseX + displacement[0]);
 }
 const CalcY = (baseY) => {
+    baseY = parseInt(baseY);
     return (-1*baseY + displacement[1]);
 }
 
 let elem = document.getElementById('world')
 
-
-const Move = (country) => {
-    elem.style.left = CalcX(country.X);
-    elem.style.top = CalcY(country.Y);
+function Move(country){
+    elem.style.left = `${CalcX(country.x)}px`;
+    elem.style.top = `${CalcY(country.y)}px`;
+    console.log(elem.style.left);
 }
 
-setTimeout(Move({x:'', y:''}), 3000);
+function Pick(){
+
+}
+
+function Display(country){
+    
+}
+
+document.getElementById('fly-button').addEventListener('click', (e)=>{
+    Move({'x':"2055",
+    "y": "1268"});
+});
