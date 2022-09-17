da1d59b04b358af86151188aa6ade076ed9c9858
nobiduke <lmjones647@gmail.com>
2022-09-16 23:29:43 -0400

    map size

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const displacement = (774, 345);
+
+let elem = document.getElementById('world')
+
 setTimeout(Move, 3000)
 
 const Move = () => {
-
+    elem.style.left = '0px'
+    elem.style.top = '0px'
 }
